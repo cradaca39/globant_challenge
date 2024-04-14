a16ecab06a125a17b49e7606194da1acfe4a6d1c
David Cartagena <davidcartagenac.cdc@gmail.com>
2024-04-13 23:12:49 -0500

    insert data jobs

--- create_db.py
+++ create_db.py
@@ -4,42 +4,20 @@ from sqlalchemy import create_engine
 from pymysql import connect
 import pandas as pd
 
-
-'''
-# connect to database
-
-data_base = connect(host = 'localhost', user = 'root', passwd = '1234')
-
-# define cursos object
-
-cur = data_base.cursor()
-
-# define de query
-
-query = 'show databases'
-
-# execute query
-
-cur.execute(query)
-'''
-
-''''
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="1234",
-  database=" globant"
-) 
-
-
-mycursor = mydb.cursor()
-'''
-#mycursor.execute("CREATE OR REPLACE TABLE departments (id INT PRIMARY KEY, department VARCHAR(255))")
-
 # Step 1: Create a DataFrame with the data
+
+# read departments
+
 df = pd.read_csv("uploads/departments.csv", delimiter=",",header=None)
 df.columns = ["id","department"]
-print(df)
+print("read departments succesful")
+
+
+# read jobs
+df_jobs = pd.read_csv("uploads/jobs.csv", delimiter=",",header=None)
+df_jobs.columns = ["id","position"]
+print("read jobs successful")
+
 
 
 # Step 2: Create a SQLAlchemy engine to connect to the MySQL database
@@ -48,8 +26,14 @@ engine = create_engine("mysql+mysqlconnector://root:1234@localhost/globant".
                                pw="1235",
                                db="globant"))
 
+
 # Step 3: Convert the Pandas DataFrame to a format for MySQL table insertion
+
+#insertion departments
 df.to_sql('departments', con=engine, if_exists='append', index=False)
+print("insert deparments sucessful")
 
-
+# insertion jobs
+df_jobs.to_sql('jobs', con=engine, if_exists='append', index=False)
+print("insert jobs sucessful")
 
